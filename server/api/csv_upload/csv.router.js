71485e6ff9043394ee31c753f5cb524c2659fd54
abhishekatxbyte <abhishek.vaghela.xbyte@gmail.com>
2023-10-19 14:12:13 +0530

    clg added

--- server/api/csv_upload/csv.router.js
+++ server/api/csv_upload/csv.router.js
@@ -13,8 +13,9 @@ const router = express.Router();
 const { csvUpload } = require('./csv.controller');
 
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' }); 
+const upload = multer({ dest: 'uploads/' });
 
 router.post('/csvupload', upload.single('file'), csvUpload);
 
 module.exports = router;
+// clg
